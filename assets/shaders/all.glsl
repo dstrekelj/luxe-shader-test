e7e469ff8a4813bb801f2660a3d763ae1d245812
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2015-07-05 22:26:49 +0200

    Tidying up functions.

--- assets/shaders/all.glsl
+++ assets/shaders/all.glsl
@@ -16,12 +16,12 @@ vec3 gb2 = vec3(139.0, 172.0, 15.0) / 256.0;
 vec3 gb3 = vec3( 48.0,  98.0, 48.0) / 256.0;
 vec3 gb4 = vec3( 15.0,  56.0, 15.0) / 256.0;
 
-vec2 scale(vec2 Scr) {
-    return gl_FragCoord.xy / Scr;
+vec2 scale() {
+    return gl_FragCoord.xy / _screen;
 }
 
-vec2 pix(vec2 Pos, vec2 Res) {
-    return floor(Pos * Res) / Res;
+vec2 pixelate(vec2 Pos) {
+    return floor(Pos * _resolution) / _resolution;
 }
 
 vec3 paletteswap(vec3 Clr) {
@@ -40,17 +40,17 @@ vec3 paletteswap(vec3 Clr) {
     return Clr;
 }
 
-vec3 vscan(vec2 Pos, vec2 Res, float Opc, vec3 Clr) {
-  Clr -= sin(Pos.y * Res.y) * Opc;
-  Clr *= sin(_time * 2.0 + Pos.y * Res.y / 120.0) * 0.18 + 0.92;
+vec3 vscan(vec2 Pos, vec3 Clr) {
+  Clr -= sin(Pos.y * _screen.y) * _opacity;
+  Clr *= sin(_time * 2.0 + Pos.y * _screen.y / 120.0) * 0.18 + 0.92;
   return Clr;
 }
 
 void main() {
-  vec2 p = scale(_screen);
+  vec2 p = scale();
   
   if (_pixelate == 1) {
-    p = pix(p, _resolution);
+    p = pixelate(p);
   }
   
   vec3 color = texture2D(tex0, p).rgb;
@@ -60,7 +60,7 @@ void main() {
   }
   
   if (_vscan == 1) {
-    color = vscan(p, _screen, _opacity, color);
+    color = vscan(p, color);
   }
   
   gl_FragColor = vec4(color, 1.0);
